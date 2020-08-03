0f5519b90461a6f98a4784599ffa9586b1824af7
Chris Fotso <chris.fotso@powerkiosk.com>
2020-08-02 19:32:00 -0500

    Updates

---
+++ precache-manifest.0170db6c8134a232a1c05c23932e60d9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d780eef93e5c50188715ad48113fdfba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3c8520822e6040c077b",
+    "url": "/static/css/main.90a6da3a.chunk.css"
+  },
+  {
+    "revision": "9bc5ab26910274610a65",
+    "url": "/static/js/2.07e3d9a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.07e3d9a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3c8520822e6040c077b",
+    "url": "/static/js/main.94551c52.chunk.js"
+  },
+  {
+    "revision": "9b03bebdb82b2536d25f",
+    "url": "/static/js/runtime-main.2cfdeec2.js"
+  },
+  {
+    "revision": "4be7899b9e75a61025e9c2337b2f5dc0",
+    "url": "/static/media/googleLibrary.4be7899b.png"
+  },
+  {
+    "revision": "4302295c9681ef191e16535eb9e35b07",
+    "url": "/static/media/portfolioSite.4302295c.png"
+  },
+  {
+    "revision": "ee5c06480aa6a3d23d806a93b79ba279",
+    "url": "/static/media/resume.ee5c0648.pdf"
+  },
+  {
+    "revision": "2e2808b48e77dbd9481b0c158d6f9572",
+    "url": "/static/media/superheroSearch.2e2808b4.png"
+  },
+  {
+    "revision": "198d6a07a4ef64821c7c596beba025c6",
+    "url": "/static/media/talentTrade.198d6a07.png"
+  },
+  {
+    "revision": "8895bf4f10f33c121480ebff4ca9babd",
+    "url": "/static/media/tidbits.8895bf4f.png"
+  }
+]);
